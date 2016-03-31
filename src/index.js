cf38cbfd0ce6e69514e4e60a642c9bd692d7bf9b
Cheton Wu <cheton@gmail.com>
2016-03-31 11:37:21 +0800

    Adds polyfill code to provide facilities that are not built into a web browser

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import events from 'events';
 import Clusterize from 'clusterize.js';
 import { flatten } from 'flattree';
 import { defaultRowRenderer } from './renderer';
+import { stopPropagation, addEventListener, removeEventListener } from './polyfill';
 import { classNames } from './utils';
 
 const extend = (target, ...sources) => {
@@ -14,42 +15,6 @@ const extend = (target, ...sources) => {
     });
 
     return target;
-};
-
-const preventDefault = (e) => {
-    if (typeof e.preventDefault !== 'undefined') {
-        e.preventDefault();
-    } else {
-        e.returnValue = false;
-    }
-};
-
-const stopPropagation = (e) => {
-    if (typeof e.stopPropagation !== 'undefined') {
-        e.stopPropagation();
-    } else {
-        e.cancelBubble = true;
-    }
-};
-
-// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Compatibility
-const addEventListener = (target, type, listener) => {
-    if (target.addEventListener) { // Standard
-        target.addEventListener(type, listener, false);
-    } else if (target.attachEvent) { // IE8
-        // In Internet Explorer versions before IE 9, you have to use attachEvent rather than the standard addEventListener.
-        target.attachEvent('on' + type, listener);
-    }
-};
-
-// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener
-const removeEventListener = (target, type, listener) => {
-    if (target.removeEventListener) { // Standard
-        target.removeEventListener(type, listener, false);
-    } else if (target.detachEvent) { // IE8
-        // In Internet Explorer versions before IE 9, you have to use detachEvent rather than the standard removeEventListener.
-        target.detachEvent('on' + type, listener);
-    }
 };
 
 class InfiniteTree extends events.EventEmitter {
