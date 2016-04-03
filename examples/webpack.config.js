ace0b2ab941d0dddcef9b2b7d5ec3ec78ccbb432
Cheton Wu <cheton@gmail.com>
2016-04-03 11:29:03 +0800

    Update webpack.config.js

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -52,10 +52,10 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
             compress: {
                 warnings: false
-            }
+            },
+            mangle: false
         })
     ],
     stylus: {
