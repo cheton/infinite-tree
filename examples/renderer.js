2524500e81254d1eeebcc0de8b54105da6a7b719
Cheton Wu <cheton@gmail.com>
2016-04-03 20:53:58 +0800

    Update API usage

--- examples/renderer.js
+++ examples/renderer.js
@@ -2,8 +2,9 @@ import { buildHTML, classNames, quoteattr } from '../src/utils';
 
 const rowRenderer = (node) => {
     const { id, label, children, state } = node;
-    const { depth, more, open, path, total, selected = false } = state;
+    const { depth, open, path, total, selected = false } = state;
     const childrenLength = Object.keys(children).length;
+    const more = node.hasChildren();
 
     let toggler = '';
     if (more) {
