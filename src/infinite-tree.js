643d9cc000b1587c8f49294c56908fc0b4ef8fae
Cheton Wu <cheton@gmail.com>
2017-04-06 12:08:38 +0800

    Update function description

--- src/infinite-tree.js
+++ src/infinite-tree.js
@@ -660,7 +660,7 @@ class InfiniteTree extends events.EventEmitter {
     // Returns the node at the specified point. If the specified point is outside the visible bounds or either coordinate is negative, the result is null.
     // @param {number} x A horizontal position within the current viewport.
     // @param {number} y A vertical position within the current viewport.
-    // @return {Node} The Node object under the given points.
+    // @return {Node} The Node object under the given point.
     getNodeFromPoint(x, y) {
         let el = document.elementFromPoint(x, y);
         while (el && el.parentElement !== this.contentElement) {
