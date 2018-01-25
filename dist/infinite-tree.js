d827e93c1d09d3a3a2053b02a5f3882ff3e6897d
Cheton Wu <cheton@gmail.com>
2018-01-25 20:01:13 +0800

    v1.14.5

--- dist/infinite-tree.js
+++ dist/infinite-tree.js
@@ -1,4 +1,4 @@
-/*! infinite-tree v1.14.4 | (c) 2018 Cheton Wu <cheton@gmail.com> | MIT | https://github.com/cheton/infinite-tree */
+/*! infinite-tree v1.14.5 | (c) 2018 Cheton Wu <cheton@gmail.com> | MIT | https://github.com/cheton/infinite-tree */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
